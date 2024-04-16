14fa61a6e173dd8721e2ad95a4af8fbde4042b9a
Sean <G00420811@atu.ie>
2024-04-16 09:34:27 +0100

    Made insert for database

--- src/main/java/ie/atu/jdbc/standard/InsertExample.java
+++ src/main/java/ie/atu/jdbc/standard/InsertExample.java
@@ -7,20 +7,23 @@ public class InsertExample {
     public static void main(String[] args) throws SQLException {
 
         // Connect to the database
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/exampledatabase", "root", "password");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mydatabasemaloney", "root", "password");
 
         try {
 
             // Insert a new record into the "users" table
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
-            stmt.setString(1, "Des");
-            stmt.setString(2, "software");
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO person (name, age, email) VALUES (?, ?, ?)");
+            stmt.setString(1, "Buddah");
+            stmt.setString(2, "60");
+            stmt.setString(3, "ghandi@englihtentment.india");
             stmt.executeUpdate();
 
             // Insert a new record into the "emails" table, referencing the new user
-            stmt = conn.prepareStatement("INSERT INTO emails (user_id, email) VALUES (?, ?)");
+            stmt = conn.prepareStatement("INSERT INTO address (person_id, street, city, county) VALUES (?, ?, ?,?)");
             stmt.setInt(1, getLastInsertId(conn));
-            stmt.setString(2, "des@atu.ie");
+            stmt.setString(2, "Enlightment road");
+            stmt.setString(3, "India");
+            stmt.setString(4, "India");
             stmt.executeUpdate();
 
             System.out.println("Insert completed successfully.");
